21065a5f3758a3c0d7a49a3927fe2b7562762cc4
dkedid <73521518+dkedid@users.noreply.github.com>
2022-02-11 18:48:29 +0900

    outline

--- src/db.js
+++ src/db.js
@@ -12,7 +12,6 @@ const handleOpen = () => {
     console.log("Connected to DB");
 };
 
-//
 const handleError = (error) => console.log(`DB error : ${error}`);
 //on은 발생할때마다 알려주고, once는 처음만 알려줌
 db.on("error", handleError);
